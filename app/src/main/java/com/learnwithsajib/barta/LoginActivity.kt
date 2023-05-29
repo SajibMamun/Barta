9ef62a00a48e276c5bdb8e8f5e76c1271ee14ff0
Sajib Bin Mamun <sajibmamun138@gmail.com>
2023-05-30 02:41:52 +0600

    navigation drawer added and fragment created

--- app/src/main/java/com/learnwithsajib/barta/LoginActivity.kt
+++ app/src/main/java/com/learnwithsajib/barta/LoginActivity.kt
@@ -1,11 +1,23 @@
 package com.learnwithsajib.barta
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.learnwithsajib.barta.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+       binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.loginbtnid.setOnClickListener {
+
+            var intent=Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/learnwithsajib/barta/MainActivity.kt
+++ app/src/main/java/com/learnwithsajib/barta/MainActivity.kt
@@ -2,7 +2,7 @@ package com.learnwithsajib.barta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+////50
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
